72055a90e06f11ffe615e70d9f03a2496ae8d6f8
vitalik-fan <vital.pereg@mail.ru>
2022-07-07 10:03:17 +0300

    more deploy

--- client/pages/lk/session/[:id].tsx
+++ client/pages/lk/session/[:id].tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../../hoc/Header";
 import Link from "next/link";
 import BackSvg from "../../../components/svg/BackSvg";
@@ -7,12 +7,12 @@ import LkFeedBackElem from "../../../components/Lk/LkFeedBackElem";
 import Chat from "../../../components/Lk/Chat";
 import copy from "./../../../assets/copy.svg";
 import axios from "axios";
-import io from 'socket.io-client';
-import {back_url, front_url} from "../../../vars";
-import {IListElem} from "../../../components/Lk/LkSessionsList";
-import {useRouter} from "next/router";
+import io from "socket.io-client";
+import { back_url, front_url } from "../../../vars";
+import { IListElem } from "../../../components/Lk/LkSessionsList";
+import { useRouter } from "next/router";
 import AnonimForm from "../../../components/Lk/AnonimForm";
-import {CopyToClipboard} from "react-copy-to-clipboard"
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import useTypedSelector from "../../../hooks/useTypedSelector";
 
 export interface IFeedBack {
@@ -27,36 +27,36 @@ export interface IFeedBack {
 }
 
 const SessionPage = () => {
-  const [session, setSession] = useState<IListElem>({} as IListElem)
-  const [feedback, setFeedback] = useState<IFeedBack[]>([])
-  const [inputValue, setInputValue] = useState<string>('')
-  const userId = useTypedSelector(state => state.user.user.id)
-  const [isCopy, setIsCopy] = useState<string>("")
-  const [isCopyFlag, setIsCopyFlag] = useState<boolean>(false)
-  const [rateProf, setRate] = useState<number>(5)
+  const [session, setSession] = useState<IListElem>({} as IListElem);
+  const [feedback, setFeedback] = useState<IFeedBack[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
+  const userId = useTypedSelector((state) => state.user.user.id);
+  const [isCopy, setIsCopy] = useState<string>("");
+  const [isCopyFlag, setIsCopyFlag] = useState<boolean>(false);
+  const [rateProf, setRate] = useState<number>(5);
 
-  const route = useRouter()
+  const route = useRouter();
 
-  console.log(feedback)
+  console.log(feedback);
 
   async function fetchSessions() {
     try {
       if (route.isReady) {
-        setIsCopy(front_url + route.asPath)
-        const id = route.asPath.split('/')[3]
-        const {data} = await axios.get(`${back_url}/session/${id}`)
-        const feedbackResponse = await axios.get(`${back_url}/message/${id}`)
-        setFeedback(feedbackResponse.data)
-        setSession(data)
+        setIsCopy(front_url + route.asPath);
+        const id = route.asPath.split("/")[3];
+        const { data } = await axios.get(`${back_url}/session/${id}`);
+        const feedbackResponse = await axios.get(`${back_url}/message/${id}`);
+        setFeedback(feedbackResponse.data);
+        setSession(data);
       }
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 
   useEffect(() => {
     fetchSessions();
-  }, [route.isReady])
+  }, [route.isReady]);
 
   const [isChatOpen, setIsChatOpen] = useState(false);
   const [whatMessageOpen, setWhatMessageOpen] = useState<number>(0);
@@ -65,40 +65,40 @@ const SessionPage = () => {
     setIsChatOpen(true);
   };
 
-  const socket = io('http://localhost:5000', { transports: ['websocket'] })
+  const socket = io(back_url, { transports: ["websocket"] });
 
   useEffect(() => {
-    socket.on('message', ({data}) => {
-      console.log(data)
-      setFeedback(prevState => [...prevState, data])
-    })
-  }, [])
+    socket.on("message", ({ data }) => {
+      console.log(data);
+      setFeedback((prevState) => [...prevState, data]);
+    });
+  }, []);
 
   useEffect(() => {
     let sum = 0;
     let howRates = 0;
-    feedback.forEach(el => {
+    feedback.forEach((el) => {
       if (el.rate) {
-        sum += Number(el.rate)
-        howRates += 1
+        sum += Number(el.rate);
+        howRates += 1;
       }
-    })
+    });
     if (howRates != 0) {
-      sum /= howRates
+      sum /= howRates;
     }
-    setRate(sum)
-  }, [feedback])
+    setRate(sum);
+  }, [feedback]);
 
   const copyHandler = () => {
-    setIsCopyFlag(true)
-    console.log(route.asPath)
-    setIsCopy(route.asPath)
-  }
+    setIsCopyFlag(true);
+    console.log(route.asPath);
+    setIsCopy(route.asPath);
+  };
 
   return (
     <Header>
-      {userId == session.userId
-        ? <>
+      {userId == session.userId ? (
+        <>
           <section className="SessionPage">
             <div className="container">
               <Link href="/lk">
@@ -118,11 +118,11 @@ const SessionPage = () => {
               <div className="dateWrap">
                 <p className="date">
                   <span>Date:</span>
-                  <span>{session.createdAt?.split('T')[0]}</span>
+                  <span>{session.createdAt?.split("T")[0]}</span>
                 </p>
                 <p className="time">
                   <span>Time:</span>
-                  <span>{session.createdAt?.split('T')[1].split('.')[0]}</span>
+                  <span>{session.createdAt?.split("T")[1].split(".")[0]}</span>
                 </p>
               </div>
               <p className="status">
@@ -132,44 +132,53 @@ const SessionPage = () => {
               {/*<button className="CloseSession">Close Session</button>*/}
               <p className="status copy">
                 <span>Copy Link:</span>
-                <CopyToClipboard text={isCopy}
-                                 onCopy={() => copyHandler()}>
-                  <img src={copy.src} alt="copy"/>
+                <CopyToClipboard text={isCopy} onCopy={() => copyHandler()}>
+                  <img src={copy.src} alt="copy" />
                 </CopyToClipboard>
-
               </p>
-              <p className="status copy StartsElem">
-                Stars: {rateProf}
-              </p>
+              <p className="status copy StartsElem">Stars: {rateProf}</p>
               <h2 className="FeedbackTitle">Feedback List:</h2>
               <ul className="FeedbackListWrapper">
-                {feedback.length != 0 ? feedback.map((feedbackElem, index) => (
-                  <li className={"LkSessionListElem"} key={"LkListElem" + index}>
-                    <LkFeedBackElem
-                      id={feedbackElem.id}
-                      name={feedbackElem.anonimName || "Anonim"}
-                      messages={feedbackElem.value}
-                      anonimId={feedbackElem.anonim || 100}
-                      rate={feedbackElem.rate}
-                      createdAt={feedbackElem.createdAt}
-                      setIsChatOpen={setIsChatOpen}
-                      isChatOpen={isChatOpen}
-                      isProfessor={feedbackElem.isProfessor}
-                      setWhatMessageOpen={setWhatMessageOpen}
-                    />
-                  </li>
-                )) :
-                <span className="NoMessages">No feedbacks yet.</span>}
+                {feedback.length != 0 ? (
+                  feedback.map((feedbackElem, index) => (
+                    <li
+                      className={"LkSessionListElem"}
+                      key={"LkListElem" + index}
+                    >
+                      <LkFeedBackElem
+                        id={feedbackElem.id}
+                        name={feedbackElem.anonimName || "Anonim"}
+                        messages={feedbackElem.value}
+                        anonimId={feedbackElem.anonim || 100}
+                        rate={feedbackElem.rate}
+                        createdAt={feedbackElem.createdAt}
+                        setIsChatOpen={setIsChatOpen}
+                        isChatOpen={isChatOpen}
+                        isProfessor={feedbackElem.isProfessor}
+                        setWhatMessageOpen={setWhatMessageOpen}
+                      />
+                    </li>
+                  ))
+                ) : (
+                  <span className="NoMessages">No feedbacks yet.</span>
+                )}
               </ul>
               {/*<h3>Send feedback</h3>*/}
               {/*<input value={inputValue} onChange={(e) => setInputValue(e.target.value)} type="text"/>*/}
               {/*<button onClick={sendMessageHandler}>Send</button>*/}
             </div>
           </section>
-          <Chat anonimId={whatMessageOpen} session={session} socket={socket} isChatOpen={isChatOpen} setIsChatOpen={setIsChatOpen} />
+          <Chat
+            anonimId={whatMessageOpen}
+            session={session}
+            socket={socket}
+            isChatOpen={isChatOpen}
+            setIsChatOpen={setIsChatOpen}
+          />
         </>
-        : <AnonimForm  socket={socket} session={session} />}
-
+      ) : (
+        <AnonimForm socket={socket} session={session} />
+      )}
     </Header>
   );
 };
